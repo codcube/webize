a654246c455420dc05569b12c74d1ff10aba1d24
user <carmen@mw.logbook.am>
2022-10-26 07:57:19 +0000

    p

--- config/scripts/site.rb
+++ config/scripts/site.rb
@@ -48,7 +48,8 @@ class WebResource
 
     Subscriptions['www.mixcloud.com'] = Webize.configList('subscriptions/mixcloud').map{|c|"https://api.mixcloud.com/#{c}/cloudcasts/"}
 
-    SC = {client_id: 'AH1HOCnlxQKahx111T3wjQlke9ceE2s9', version: 1663668775 }
+    SC = {client_id: '3lMNQuShZSKBH6Xx1bRzEBCa1TWoa3ui',
+          version: 1666686311}
 
     Subscriptions['soundcloud.com'] = Webize.configList('subscriptions/soundcloud').map{|chan|
       "https://api-v2.soundcloud.com/stream/users/#{chan}?client_id=#{SC[:client_id]}&limit=20&offset=0&linked_partitioning=1&app_version=#{SC[:version]}&app_locale=en"}
