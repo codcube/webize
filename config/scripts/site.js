3bcad923f2fcea2f0b32303528e8eae8d2523655
user <carmen@mw.logbook.am>
2023-05-13 19:31:58 +0000

    p

--- config/scripts/site.js
+++ config/scripts/site.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function(){
     // construct selection ring
     var first = null;
     var last = null;
-    document.querySelectorAll('a[id], div[id], h1[id], h2[id], h3[id], p[id], section[id]').map(function(e){
+    document.querySelectorAll('a.title[id], .toolbox a[id]').map(function(e){
 	if(!first)     // first node
 	    first = this;	
 	if(last){      // link to previous node
