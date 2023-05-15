99b700ce370b39d06a0f571943c4d97ee7f7907c
user <carmen@mw.logbook.am>
2023-05-15 15:38:59 +0000

    p

--- config/scripts/site.rb
+++ config/scripts/site.rb
@@ -27,10 +27,9 @@ class WebResource
     Subscriptions['www.mixcloud.com'] = Webize.configList('subscriptions/mixcloud').map{|c|
       "https://api.mixcloud.com/#{c}/cloudcasts/"}
 
-    SC = {client_id: 'nBNZK2jas9ofvx4mqT6KYcUoDFeOdlRn', version: 1680773289}
-
+    SoundcloudTokens = Webize.configHash 'tokens/soundcloud'
     Subscriptions['soundcloud.com'] = Webize.configList('subscriptions/soundcloud').map{|chan|
-      "https://api-v2.soundcloud.com/stream/users/#{chan}?client_id=#{SC[:client_id]}&limit=20&offset=0&linked_partitioning=1&app_version=#{SC[:version]}&app_locale=en"}
+      "https://api-v2.soundcloud.com/stream/users/#{chan}?client_id=#{SoundcloudTokens['client_id']}&limit=20&offset=0&linked_partitioning=1&app_version=#{SoundcloudTokens['version']}&app_locale=en"}
 
     Subscriptions['www.youtube.com'] = Webize.configList('subscriptions/youtube').map{|c|
       'https://www.youtube.com/feeds/videos.xml?channel_id=' + c}
@@ -91,6 +90,7 @@ class WebResource
         r.deny
      end}
 
+    GET 'twitter.com', -> r {[301, {'Location' => ['//nitter.net',  r.path,  '?', r.query].join.R(r.env).href}, []]}
     GET 'wiki.c2.com', -> r {['https://proxy.c2.com/wiki/remodel/pages/', r.env['QUERY_STRING']].join.R(r.env).fetchHTTP}
 
     GET 'www.youtube.com', -> r {
