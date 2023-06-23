ecd869fb744122d077541d13918f55022690a9db
user <carmen@mw.logbook.am>
2023-06-23 00:20:05 +0000

    p

--- Formats/POSIX.rb
+++ Formats/POSIX.rb
@@ -12,7 +12,7 @@ class WebResource
   module HTML
 
     ContainerStyle = {
-      '#updates' => 'background: repeating-linear-gradient(135deg, #fff, #fff .5em, #000 .5em, #000 1em); '}
+      '#updates' => 'background: repeating-linear-gradient(320deg, #fff, #fff .8em, #000 .8em, #000 1em); '}
 
     Markup['http://www.w3.org/ns/ldp#Container'] = -> dir, env {
       content = dir.delete('http://www.w3.org/ns/ldp#contains') || []
