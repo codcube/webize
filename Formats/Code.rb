7fbb74a5b680eabfebdc57ef92c0da0f533d648c
user <carmen@mw.logbook.am>
2023-09-26 07:05:14 +0000

    p

--- Formats/Code.rb
+++ Formats/Code.rb
@@ -7,11 +7,8 @@ module Webize
     class Format < RDF::Format
       content_type 'application/ruby',
                    aliases: %w(
-                   application/javascript;q=0.2
-                   application/x-javascript;q=0.2
                    application/x-sh;q=0.2
                    text/css;q=0.2
-                   text/javascript;q=0.8
                    text/x-c;q=0.8
                    text/x-perl;q=0.8
                    text/x-ruby;q=0.8
@@ -19,7 +16,7 @@ module Webize
                    text/x-shellscript;q=0.8
                    text/yaml;q=0.8
                    ),
-                   extensions: [:bash, :c, :css, :cpp, :erb, :gemspec, :go, :h, :hs, :js, :mk, :nim, :nix, :patch, :pl, :pm, :proto, :py, :rb, :sh, :yaml, :zsh]
+                   extensions: [:bash, :c, :css, :cpp, :erb, :gemspec, :go, :h, :hs, :mk, :nim, :nix, :patch, :pl, :pm, :proto, :py, :rb, :sh, :yaml, :zsh]
       content_encoding 'utf-8'
       reader { Reader }
     end
@@ -30,7 +27,6 @@ module Webize
       def initialize(input = $stdin, options = {}, &block)
         @base = options[:base_uri]
         @doc = (input.respond_to?(:read) ? input.read : input).encode 'UTF-8', undef: :replace, invalid: :replace, replace: ' '
-
         if block_given?
           case block.arity
           when 0 then instance_eval(&block)
@@ -47,20 +43,46 @@ module Webize
           fn.call RDF::Statement.new(@base, p, o, :graph_name => @base)}
       end
 
-      def source_tuples
-        # Rouge
-        if lexer = Rouge::Lexer.guess_by_filename(@base.basename) rescue nil
-          html = Rouge::Formatters::HTMLPygments.new(Rouge::Formatters::HTML.new).format(lexer.lex(@doc))
+      def highlight
+        # Rouge setup
+        if lexer = Rouge::Lexer.find_fancy(@base.extname) rescue nil
+          Rouge::Formatters::HTMLPygments.new(Rouge::Formatters::HTML.new).format(lexer.lex(@doc))
         else
-           logger.warn "can't determine lexer for #{@base}"
+          logger.warn "can't determine lexer for #{@base}"
         end
+      end
 
-        html = RDF::Literal [html,
+      def source_tuples
+        html = RDF::Literal [highlight,
                              '<style>', CSS::Code, '</style>'
                             ].join.encode 'UTF-8', undef: :replace, invalid: :replace, replace: ' '
         html.datatype = RDF.XMLLiteral
         yield Content.R, html
       end
+    end
+  end
+
+  module JS
+
+    class Format < Code::Format
+      content_type 'application/javascript',
+                   aliases: %w(
+                   application/x-javascript;q=0.2
+                   text/javascript;q=0.8
+                   ),
+                   extensions: [:js]
+      content_encoding 'utf-8'
+
+      reader { Reader }
+    end
+
+    class Reader < Code::Reader
+
+      def highlight
+        lexer = Rouge::Lexers::Javascript.new
+        Rouge::Formatters::HTMLPygments.new(Rouge::Formatters::HTML.new).format(lexer.lex(@doc))
+      end
+
     end
   end
 end
