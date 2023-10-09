1428e98f3391e2397930e79e4f6b7c14e1578bd1
user <carmen@mw.logbook.am>
2023-10-09 01:09:32 +0000

    pg

--- Formats/RDF.rb
+++ Formats/RDF.rb
@@ -9,8 +9,8 @@ module Webize
   Dir.children([ConfigPath, VocabPath].join '/').map{|vocab|                # find vocab
     if vocabulary = vocab == 'rdf' ? {uri: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'} : RDF.vocab_map[vocab.to_sym] # vocabulary prefix
       Dir.children([ConfigPath, VocabPath, vocab].join '/').map{|predicate| # find predicates
-        destURI = [vocabulary[:uri], predicate].join
-        configList([VocabPath, vocab, predicate].join '/').map{|srcURI|     # parse mapping
+        destURI = [vocabulary[:uri], predicate].join                        # expand predicate URI
+        configList([VocabPath, vocab, predicate].join '/').map{|srcURI|     # parse mapping entries
           MetaMap[srcURI] = destURI}}                                       # map predicate
     else
       Console.logger.warn "❓ undefined prefix #{vocab} referenced by vocab map"
