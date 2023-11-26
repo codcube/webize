83d3477736fa7fbe37ea630c24c6a8252d7f2dac
user <carmen@mw.logbook.am>
2023-11-26 23:20:06 +0000

    p

--- Protocols/DNS.rb
+++ Protocols/DNS.rb
@@ -7,6 +7,7 @@ class FilteredServer < Async::DNS::Server
   DefaultAddr = ENV['ADDR'] || '127.0.0.1'
 
   Log = -> name, color {
+    v6 = resource_class == Resolv::DNS::Resource::IN::AAAA
     puts [Time.now.iso8601[11..15], v6 ? '6️⃣' : nil, [color, "\e]8;;https://#{name}/\a#{name}\e]8;;\a\e[0m"].join].compact.join ' '
   }
 
@@ -31,25 +32,35 @@ class FilteredServer < Async::DNS::Server
                                              [:tcp, '2001:4860:4860::8844', 53],
                                            ])
 
-    v6 = resource_class == Resolv::DNS::Resource::IN::AAAA
     resource = Webize::URI(['//', name].join)
 
     if resource.deny?
       color = "\e[38;5;#{resource.deny_domain? ? 196 : 202};7m"
-      Log[name, color]
+      Log[name, resource_class, color]
       transaction.respond! v6 ? '::1' : DefaultAddr
     else
       color = name.index('www.') == 0 ? nil : "\e[38;5;51m"
-      Log[name, color]
+      Log[name, resource_class, color]
       transaction.passthrough! @resolver
     end
   end
 end
 
-# if binding isn't allowed, you can enable it by running on (linux-compatible) OS:
-#  sudo setcap 'cap_net_bind_service=+ep' /usr/bin/ruby
-# or change the port to a high (>1024) and,
-# redirect 53 to say 1053 or 5300 via the kernel tables by running the ../low_ports script, or redirect the traffic via a 'sudo socat' listener, or try a direct high-port specification in /etc/resolv.conf or your system resolver settings
+## Listener
+
+# if binding isn't allowed, the minimalist solution is:
+
+# 1) use a high-port resolver specification in /etc/resolv.conf or your system resolver settings
+
+# or you can enable low-port binding on a linux-compatible OS by running:
+
+# 2) sudo setcap 'cap_net_bind_service=+ep' /usr/bin/ruby
+
+# or change the below port to high (>1024) and,
+
+# 3) redirect port 53 to say 1053 or 5300 via kernel tables by running the <../low_ports> script, or
+# 4) redirect traffic in userspace with a 'sudo socat' instantiation
+
 server = FilteredServer.new([[:udp, '127.0.0.1', 53],
                              [:tcp, '127.0.0.1', 53],
                              [:udp, '::1', 53],
