5edb66ddf916419c1712ee15d6e5e5d1e043f9ad
user <carmen@mw.logbook.am>
2023-11-26 22:36:58 +0000

    p

--- Protocols/DNS.rb
+++ Protocols/DNS.rb
@@ -3,11 +3,22 @@ require 'async/dns'
 
 class FilteredServer < Async::DNS::Server
   def process(name, resource_class, transaction)
-    @resolver ||= Async::DNS::Resolver.new([[:udp, '8.8.8.8', 53], [:tcp, '8.8.8.8', 53]])
-    puts name,resource_class,transaction
+    @resolver ||= Async::DNS::Resolver.new([#[:udp, '1.1.1.1', 53],
+                                            #[:tcp, '1.1.1.1', 53],
+                                            #[:udp, '8.8.8.8', 53],
+                                            #[:tcp, '8.8.8.8', 53],
+                                            [:udp, '2001:4860:4860::8888', 53],
+                                            [:udp, '2001:4860:4860::8844', 53],
+                                            [:udp, '2606:4700:4700::1111', 53],
+                                            [:udp, '2606:4700:4700::1001', 53],])
+    #transaction.respond!("1.2.3.4")
+    puts name.class,name
     transaction.passthrough!(@resolver)
   end
 end
 
-server = FilteredServer.new([[:udp, '127.0.0.1', 53]])
+server = FilteredServer.new([#[:udp, '127.0.0.1', 53],
+                             #[:tcp, '127.0.0.1', 53],
+                             [:udp, '::1', 53],
+                             [:tcp, '::1', 53]])
 server.run
