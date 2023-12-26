59ae7646751c7a1f6d3433f155eba4b30494926c
user <carmen@mw.logbook.am>
2023-12-26 17:25:10 +0000

    pp

--- Formats/HTML.write.rb
+++ Formats/HTML.write.rb
@@ -102,9 +102,9 @@ module Webize
                       '#f00'
                     else
                       env[:warnings].push ['pattern block in URI<br>',
-                                           "<span style='background-color: #ddd; font-size: .8em'>",
+                                           "<span style='background-color: #ddd; font-size: .88em'>",
                                            uri.gsub(Webize::Gunk){|m|
-                                             ['<b style="font-size:1.5em; background-color: #fff">', m, '</b>'].join },
+                                             ['<b style="font-size:1.3em; background-color: #fff">', m, '</b>'].join },
                                            '</span>']
                       '#f80'
                     end
