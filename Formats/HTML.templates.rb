c047a33128303bc0b54a223491cc50012dc75ea5
user <carmen@mw.logbook.am>
2024-06-26 14:36:50 +0000

    p

--- Formats/HTML.templates.rb
+++ Formats/HTML.templates.rb
@@ -216,7 +216,8 @@ module Webize
              {_: :span, class: :count, c: counter[Schema+'userInteractionCount']}]}
       end
 
-      def resource r, name = :div
+      def resource r, element_type = nil
+        r.delete Type if element_type # explicit element type
 
         # predicate renderer lambda
         p = -> a {property(a, r.delete(a)) if r.has_key? a}
@@ -240,7 +241,7 @@ module Webize
                   Webize::URI.new(r[To][0]).display_name)[0..5] if r.has_key?(To) &&
                                                                    r[To].size==1 &&
                                                                    Resources.member?(r[To][0].class)
-        {_: name, class: :resource,                # resource representation
+        {_: element_type, class: :resource,        # resource representation
          c: [({class: :title, c: p[Title]}.        # title
                 update(ref || {}) if r.has_key? Title),
              p[Abstract], p[To],                   # abstract, dest
