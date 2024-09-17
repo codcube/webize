8e0228104fd6d25b8ac2dfea9e8addd35b7caffc
user <carmen@mw.logbook.am>
2024-09-17 04:30:52 +0000

    p

--- Protocols/POSIX/names.rb
+++ Protocols/POSIX/names.rb
@@ -53,9 +53,9 @@ module Webize
 
     # create containing dir(s)
     def mkdir
-      dir = cursor = dirURI? ? fsPath.sub(/\/$/,'') : File.dirname(fsPath) # strip slash from cursor (blocking filename doesn't have one)
+      dir = cursor = dirURI? ? fsPath.sub(/\/$/,'') : File.dirname(fsPath) # strip slash from cursor (any blocking filename won't have one)
       until cursor == '.'                # cursor at root?
-        if File.file?(cursor) || File.symlink?(cursor)
+        if File.file? cursor
           FileUtils.rm cursor            # unlink file/link blocking location
           puts '🧹 ' + cursor            # log fs-sweep
         end
