bbdeadca904ebda69cba602cc4520b0ee49d1b26
user <carmen@mw.logbook.am>
2024-09-18 00:04:19 +0000

    p

--- Protocols/HTTP/rack.rb
+++ Protocols/HTTP/rack.rb
@@ -112,28 +112,30 @@ module Webize
   class HTTP::Node
 
     def GET
-      return hostGET if host                  # remote node
+      return hostGET if host     # remote node
 
-      ps = parts                              # path nodes
-      p = ps[0]                               # first node
+      ps = parts                 # path nodes
+      p = ps[0]                  # first node
 
-      return fetchLocal unless p              # local node - empty or root path
-      return unproxy.hostGET if p[-1] == ':' && ps.size > 1        # remote node - proxy URI with scheme
-      return unproxy.hostGET if p.index('.') && p != 'favicon.ico' # remote node - proxy URI sans scheme
-      return dateDir if %w{m d h y}.member? p # current year/month/day/hour container
+      return fetchLocal unless p # local node - void or root path
+                                 # proxy URI
+      return unproxy.hostGET if (p[-1] == ':' && ps.size > 1) || # remote node, URI w/ scheme
+                                (p.index('.') && p != 'favicon.ico') #            sans scheme
+
+      return dateDir if %w{m d h y}.member? p # current year/month/day/hour contents
       return block parts[1] if p == 'block'   # block domain
-      return redirect '/d?f=msg*' if path == '/mail' # email inbox
+      return redirect '/d?f=msg*' if path == '/mail' # email
 
-      if extname == '.u'                      # URI list
+      if extname == '.u'         # URI list
         case query
         when 'fetch'
-          return fetch uris                   # remote node(s)
+          return fetch uris      # remote node(s)
         when 'load'
-          return fetchLocal uris              # cached node(s)
+          return fetchLocal uris # cached node(s)
         end
       end
 
-      fetchLocal                              # local node(s)
+      fetchLocal                 # local node(s)
     end
 
     def OPTIONS
