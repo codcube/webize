12f99577331ea8afc53e086eafd1b8e8e08d48d0
user <carmen@mw.logbook.am>
2024-09-20 17:42:56 +0000

    p

--- Formats/HTML.rb
+++ Formats/HTML.rb
@@ -1,2 +1,5 @@
 %w(read write template).map{|s|
   require_relative "HTML/#{s}.rb"}
+
+# it's still picking the stock HTML/RDFa parsers/serializers sometimes. this may reduce the frequency, or cause other errors if something expects it to exist. we'll find out
+RDF::Format.content_types['text/html'] -= [RDF::RDFa::Format]
