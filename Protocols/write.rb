b0a68277113d47360dab6ee0612125c04dafeca2
user <carmen@mw.logbook.am>
2024-09-21 20:44:26 +0000

    p

--- Protocols/write.rb
+++ Protocols/write.rb
@@ -68,7 +68,7 @@ module Webize
 
           host = RDF::URI('//' + (g.host || 'localhost'))                      # host container
           summary << RDF::Statement.new(env[:base], RDF::URI(Contains), host)  # base 👉 host container
-          summary << RDF::Statement.new(host, RDF::URI(Title), g.display_host) # host label
+          summary << RDF::Statement.new(host, RDF::URI(Title), g.display_host || 'localhost') # host label
           summary << RDF::Statement.new(host, RDF::URI('#graph'), g)           # host container 👉 summarized graph
           summaries << summary                                                 # summary graph of graphs
         else
