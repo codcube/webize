8de90118c8ef1d28408078a65436816fcb6f58ad
user <carmen@mw.logbook.am>
2024-09-22 06:24:40 +0000

    p

--- Protocols/POSIX/io.rb
+++ Protocols/POSIX/io.rb
@@ -35,7 +35,7 @@ module Webize
 
         graph << RDF::Statement.new(c, RDF::URI(Title), name)
 
-        if nodes.size > 32 # alpha binning of large directories - TODO generic alpha binning in graph-summarizer
+        if nodes.size > 96 # alpha binning of large directories
           char = c.basename[0].downcase
           bin = Node join char + '*'
           graph << RDF::Statement.new(self, RDF::URI(Contains), bin)
--- Protocols/read.rb
+++ Protocols/read.rb
@@ -17,11 +17,10 @@ module Webize
         repository << RDF::Statement.new(self, RDF::URI(Title), basename)
       else
         if reader ||= RDF::Reader.for(content_type: format)       # if reader exists for format:
-          base = self                                             # provide doc or request URI as most formats lack HTML/Turtle-style base declaration facilities
-          r = reader.new(content, base_uri: base){|_| repository << _ }            # read RDF
-#          if base != r.base_uri                                                    # base URI override by document declaration?
-#            repository << RDF::Statement.new(base, RDF::URI(Contains), r.base_uri) # reference non-canonical base
-#          end
+          r = reader.new(content, base_uri: self){|_| repository << _ }            # read graph
+          if self != r.base_uri                                                    # base URI override by document declaration?
+            repository << RDF::Statement.new(env[:base], RDF::URI(Contains), r.base_uri) # reference graph base from canonical base
+          end
         else
           logger.warn ["⚠️ no RDF reader for " , format].join
         end
