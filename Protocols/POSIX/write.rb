bb2cd033889650673142c545996beaf8e064bc8c
user <carmen@mw.logbook.am>
2024-10-07 13:13:04 +0000

    p

--- Protocols/POSIX/write.rb
+++ Protocols/POSIX/write.rb
@@ -1,7 +1,7 @@
 module Webize
   class POSIX::Node
 
-    # make containing dir(s) with pre-cleanup for files in name slots where dirs are going to go
+    # sweep blocking files where dirs are going to go
     def mkdir
       dir = cursor = dirURI? ? fsPath.sub(/\/$/,'') : File.dirname(fsPath) # strip slash from cursor (any blocking filename won't have one)
       until cursor == '.'                # cursor at root?
