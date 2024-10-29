6738249c0de5a9e1ebe48d39779df717f9ab3270
user <carmen@mw.logbook.am>
2024-10-29 14:50:23 +0000

    p

--- Formats/HTML/template/graphic.rb
+++ Formats/HTML/template/graphic.rb
@@ -5,22 +5,26 @@ module Webize
       Markup[Image] = :img
 
       def img images
+        # all objects of this predicate considered an image.
+        # this is a common pattern in JSON where the image URI is in a string value or object without type info alongside it. referring-context is more explicit than extension-sniffing heuristics which miss any image without a classic fs-name extension - quite common now with content-addressing / hash-based CDN URLs, specialized image-servers etc. the predicate URI says it's an image so we'll take its word and bypass the object type dispatcher
         images.map do |i|
           Node.new(env[:base]).env(env).img i
         end
       end
-
     end
     class Node
 
       Markup[Image] = :img
 
-      def img image
+      def img image, container: false
+        # container: indirect @src reference in RDF: <#container> <dc:Image> <imageURI>
+        # no-container: direct @src reference at resource URI
         return unless image.class == Hash
 
-        i = Webize::Resource env[:base].join(image['uri']), env if image.has_key? 'uri' # identity
+        # identifier of container or image
+        i = Webize::Resource env[:base].join(image['uri']), env if image.has_key? 'uri'
 
-        [{_: :span, class: :image, # wrapping node for image and metadata nodes
+        [{_: :span, class: :image, # container for <img> and metadata fields
           c: [
             if i
               if i.deny?
@@ -39,6 +43,9 @@ module Webize
            update(i ? {id: i.local_id} : {}),
          ' ']
       end
+
+      # <img>
+      def imageContainer(i) = img i, container: true
 
       def figure(f) = inlineResource f, :figure
       def picture(p) = inlineResource p, :span
