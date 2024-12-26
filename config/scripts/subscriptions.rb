6e18f228aacc547e62ec9a059d0b63c0f3feaf53
user <carmen@mw.logbook.am>
2024-12-26 09:05:03 +0000

    remove /feed endpoint which introduced inconsistency between proxied an origin (where it does not exist) URL resolvability. you need to run config/soundcloud script every 2 mnonths when teh token rolls now

--- config/scripts/subscriptions.rb
+++ config/scripts/subscriptions.rb
@@ -1,22 +1,12 @@
 module Webize
   module Feed
 
-    # mixcloud
-    subscribe 'www.mixcloud.com' do |chan|
-      "https://api.mixcloud.com/#{chan}/cloudcasts/"
-    end
-
     # soundcloud
     SoundcloudTokens = Webize.configHash 'tokens/soundcloud'
 
     subscribe 'soundcloud.com' do |chan|
 
       "https://api-v2.soundcloud.com/stream/users/#{chan}?client_id=#{SoundcloudTokens['client_id']}&limit=20&offset=0&linked_partitioning=1&app_version=#{SoundcloudTokens['version']}&app_locale=en"
-    end
-
-    # youtube
-    subscribe 'www.youtube.com' do |chan|
-      'https://www.youtube.com/feeds/videos.xml?channel_id=' + chan
     end
 
   end
