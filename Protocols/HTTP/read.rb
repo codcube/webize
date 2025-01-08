d192829e1142339cee2070fc324c284ef6ef0dba
user <carmen@mw.logbook.am>
2025-01-08 09:10:58 +0000

    p

--- Protocols/HTTP/read.rb
+++ Protocols/HTTP/read.rb
@@ -233,8 +233,9 @@ module Webize
       env[:warnings].push [e.class,                     # error class
                            {_: :a, href: href, c: uri}, # request URI
                            (CGI.escapeHTML e.message),  # error message
-                           {_: :pre,                    # error backtrace
-                            c: (CGI.escapeHTML e.backtrace.join "\n")}]
+                           #{_: :pre,                    # error backtrace
+                           #c: (CGI.escapeHTML e.backtrace.join "\n")}
+                          ]
       opts[:thru] == false ? nil : notfound
     end
 
