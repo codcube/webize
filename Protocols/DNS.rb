51e6dcd4c58f7a6bb65f2909d281650aa9cc06df
user <carmen@mw.logbook.am>
2025-10-07 21:12:40 +0000

    p

--- Protocols/DNS.rb
+++ Protocols/DNS.rb
@@ -6,26 +6,37 @@ class Webize::DNS < Async::DNS::Server
 
   Seen = {} # hosts we've encountered in current run
 
-  Log = -> name, color, v6 {
-    unless Seen[name]
-      Seen[name] = true
+  Log = -> resource {
+    unless Seen[resource.host]
+      Seen[resource.host] = true # mark as visited
+
+      Graph << RDF::Statement.new(RDF::URI(Time.now.utc.strftime '/%Y/%m/%d/%H/'), RDF::URI('#nslookup'), resource) # populate runtime graph cache
+
+      # logging
+      color = if resource.deny?
+                "\e[38;5;#{resource.deny_domain? ? 196 : 202};7m"
+              else
+                "\e[38;5;51m"
+              end
       puts [Time.now.iso8601[11..15],
-            v6 ? '6️' : nil,
-            [color, "\e]8;;https://#{name}/\a#{name}\e]8;;\a\e[0m"].join].
+            [color, "\e]8;;https://#{resource.host}/\a#{resource.host}\e]8;;\a\e[0m"].join].
              compact.join ' '
     end}
 
   def resolver = @resolver ||= Async::DNS::Resolver.new(Async::DNS::Endpoint.for('1.1.1.1'))
 
   def process(name, resource_class, transaction)
-    v6 = resource_class == Resolv::DNS::Resource::IN::AAAA
-    if (resource = Webize::URI ['//', name].join).deny?
-      color = "\e[38;5;#{resource.deny_domain? ? 196 : 202};7m"
-      Log[name, color, v6]
-      transaction.respond! v6 ? '::1' : '127.0.0.1'
+    resource = Webize::URI ['//', name].join # host URI
+    Log[resource]                            # log request
+                                             # response
+    if resource.deny?
+      # BLOCK
+      # respond with IPv4/6 localhost address of proxy/tunnel listeners for filtered egress
+      # note we previously supported ENV config of proxy address but this wasn't used because browsers/clients offer this option scoped to themselves where it won't by neccessity apply to every program's requests. if you want global proxy settings without requiring client config, the feature could be added back here
+      transaction.respond! resource_class == Resolv::DNS::Resource::IN::AAAA ? '::1' : '127.0.0.1'
     else
-      color = name.index('www.') == 0 ? nil : "\e[38;5;51m"
-      Log[name, color, v6]
+      # ALLOW
+      # pass through DNS (+ therefore HTTP) requests to origin server
       transaction.passthrough! resolver
     end
   end
